95d16b9e3ee0a39bbd7322632b16d3165a1718d8
Thomas Hunter II <tlhunter@datadog.com>
2025-12-10 15:07:11 -0800

    linter

--- packages/datadog-plugin-ws/src/util.js
+++ packages/datadog-plugin-ws/src/util.js
@@ -9,33 +9,28 @@ const crypto = require('crypto')
  * @returns {string|undefined} A 32-character hash uniquely identifying the message, or undefined if unable to hash
  */
 function generateWebSocketMessageHash (data) {
-  try {
-    let dataToHash
+  let dataToHash
 
-    if (typeof data === 'string') {
-      dataToHash = data
-    } else if (Buffer.isBuffer(data)) {
-      dataToHash = data
-    } else if (data instanceof ArrayBuffer) {
-      dataToHash = Buffer.from(data)
-    } else if (data instanceof Blob) {
-      // For Blobs, we can't synchronously get the content
-      // Skip hashing for now
-      return
-    } else if (Array.isArray(data)) {
-      // Handle array of buffers
-      dataToHash = Buffer.concat(data.map(chunk => Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk)))
-    } else {
-      // Unknown data type
-      return
-    }
-
-    const hash = crypto.createHash('sha256').update(dataToHash).digest('hex')
-    return hash.slice(0, 32)
-  } catch {
-    // If hashing fails for any reason, return undefined
+  if (typeof data === 'string') {
+    dataToHash = data
+  } else if (Buffer.isBuffer(data)) {
+    dataToHash = data
+  } else if (data instanceof ArrayBuffer) {
+    dataToHash = Buffer.from(data)
+  } else if (data instanceof Blob) {
+    // For Blobs, we can't synchronously get the content
+    // Skip hashing for now
+    return
+  } else if (Array.isArray(data)) {
+    // Handle array of buffers
+    dataToHash = Buffer.concat(data.map(chunk => Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk)))
+  } else {
+    // Unknown data type
     return
   }
+
+  const hash = crypto.createHash('sha256').update(dataToHash).digest('hex')
+  return hash.slice(0, 32)
 }
 
 module.exports = {
